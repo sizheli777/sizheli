fae156b7482123a5372c25898cac25e0fb0f6005
sizheli777 <1553829330@qq.com>
2026-03-03 20:35:56 +0800

    Create queens.py
    feat: implement n-queens solver

---
+++ hw01/src/queens.py
@@ -0,0 +1,24 @@
+def solve_n_queens(n):
+    solutions = []
+    board = [-1] * n
+
+    def is_valid(row, col):
+        for i in range(row):
+            if board[i] == col or \
+               board[i] - i == col - row or \
+               board[i] + i == col + row:
+                return False
+        return True
+
+    def backtrack(row):
+        if row == n:
+            solutions.append(board[:])
+            return
+        for col in range(n):
+            if is_valid(row, col):
+                board[row] = col
+                backtrack(row + 1)
+                board[row] = -1
+
+    backtrack(0)
+    return solutions
