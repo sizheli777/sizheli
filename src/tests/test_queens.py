c9c6dadc1d42077bb271ddde9894613e1c404809
sizheli777 <1553829330@qq.com>
2026-03-03 20:28:26 +0800

    Create test_queens.py
    test: add unit tests

---
+++ src/tests/test_queens.py
@@ -0,0 +1,11 @@
+from src.queens import solve_n_queens
+
+
+def test_n4():
+    solutions = solve_n_queens(4)
+    assert len(solutions) == 2
+
+
+def test_n8():
+    solutions = solve_n_queens(8)
+    assert len(solutions) == 92
